c51bdfecf6e06598af8055077e3eb4a7820a7508
sarn <sarn@tyndale.com>
2017-12-04 14:49:01 -0600

    code example - hash

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Treehouse
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student() { Name = "Sally", GradeLevel = 3 },
+                new Student() { Name = "Bob", GradeLevel = 3 },
+                new Student() { Name = "Sally", GradeLevel = 2 }
+            };
+
+            SchoolRoll schoolRoll = new SchoolRoll();
+            schoolRoll.AddStudents(students);
+
+            schoolRoll.AddStudents(students);
+
+            foreach (Student student in schoolRoll.Students)
+            {
+                Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/SchoolRoll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Treehouse
+{
+    class SchoolRoll
+    {
+        private HashSet<Student> _students = new HashSet<Student>();
+
+        public IEnumerable<Student> Students { get { return _students; } }
+
+        public void AddStudents(IEnumerable<Student> students)
+        {
+            _students.UnionWith(students);
+        }
+    }
+}
